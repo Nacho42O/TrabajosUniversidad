ff6787b3f50497e417f7841c28d2370e8de32661
Nacho42O <ignaciov1405@gmail.com>
2024-09-26 17:41:02 -0300

    first commit

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className = "flex justify-center mb-4">
+      <h1>  First Crud Page</h1>
+      <div>
+        <div>
+          <ul>
+            <li>Tirle:</li>
+            <li>Description:</li>
+          </ul>
+        </div>
+        </div>    
+  </div>
+  );
+}
